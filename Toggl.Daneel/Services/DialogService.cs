7c2c7942194af7b8b3846dafa2ef1548a8d577aa
Arturs Eimanis <arturseimanis@gmail.com>
2017-10-20 11:55:36 +0300

    :construction: Add dialog service

---
+++ Toggl.Daneel/Services/DialogService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Toggl.Foundation.MvvmCross.Services;
+using UIKit;
+
+namespace Toggl.Daneel.Services
+{
+    public class DialogService : IDialogService
+    {
+        public void Confirm(
+            string title,
+            string message,
+            string confirmButtonTitle,
+            string dismissButtonTitle,
+            Action confirmAction,
+            Action dismissAction)
+        {
+            var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create(confirmButtonTitle, UIAlertActionStyle.Default, _ => confirmAction?.Invoke()));
+            alert.AddAction(UIAlertAction.Create(dismissButtonTitle, UIAlertActionStyle.Cancel, _ => dismissAction?.Invoke()));
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+        }
+
+        public void ShowMessage(string title, string message, string dismissButtonTitle, Action dismissAction)
+        {
+            var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create(dismissButtonTitle, UIAlertActionStyle.Cancel, _ => dismissAction?.Invoke()));
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+        }
+    }
+}
