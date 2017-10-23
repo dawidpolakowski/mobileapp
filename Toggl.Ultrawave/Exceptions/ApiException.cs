4eb24712542b5ed48fd24f8658e31bb4b344f77b
Simon Rozsival <simon@rozsival.com>
2017-10-23 12:17:36 +0200

    :sparkles: Add more information to API exceptions

--- Toggl.Ultrawave/Exceptions/ApiException.cs
+++ Toggl.Ultrawave/Exceptions/ApiException.cs
@@ -1,12 +1,31 @@
-﻿﻿using System;
+﻿using System;
+using Toggl.Ultrawave.Network;
 
 namespace Toggl.Ultrawave.Exceptions
 {
     public class ApiException : Exception
     {
-        public ApiException(string errorMessage)
-            : base(errorMessage)
+        private readonly IRequest request;
+
+        private readonly IResponse response;
+
+        private string message;
+
+        internal ApiException(IRequest request, IResponse response, string message)
         {
+            this.request = request;
+            this.response = response;
+            this.message = message;
         }
+
+        public override string ToString()
+            => $"ApiException for request {request.HttpMethod} {request.Endpoint}:"
+                + $"Response:"
+                + $"(Status code [{response.StatusCode}])"
+                + $"(Headers: {response.StatusCode})"
+                + $"(Body: {response.RawData})"
+                + $"(Message: {message})";
+
+        public override string Message => ToString();
     }
 }
